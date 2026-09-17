b6f82756809f1d82e62a1e9b812c29d88f1ff489
molotov <molotovch@protonmail.com>
2026-09-17 03:20:58 +0200

    added favicon

--- post.php
+++ post.php
@@ -61,6 +61,7 @@ $postDate = $dtObj ? $dtObj->format('d/m/Y H:i:s') : '—';
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
     <title>molotovs website</title>
+    <link rel="icon" type="image/x-icon" href="/img/favicon.ico">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI"
         crossorigin="anonymous"></script>
